e603f4e65e048cca5cf316d6d52aa869ba2b6b73
Vikash Kumar <vikashummadi@gmail.com>
2025-05-20 11:59:20 +0530

    latest

--- app.js
+++ app.js
@@ -2,31 +2,34 @@ let btn = document.querySelector("button");
 let ul = document.querySelector("ul");
 let inp = document.querySelector("input");
 
-btn.addEventListener("click", function() {
-  if(inp.value == ""){
+btn.addEventListener("click", function () {
+  if (inp.value == "") {
     return;
   }
   let item = document.createElement("li");
   item.innerText = inp.value;
   item.style.color = "red";
+  item.style.width = "250px";
+  item.style.paddingTop = "10px";
+  item.style.paddingBottom = "10px";
   let delbtn = document.createElement("button");
   delbtn.innerHTML = `<i class="fa-solid fa-x"></i>`;
-  delbtn.style.borderRadius= "7px";
+  delbtn.style.borderRadius = "7px";
   delbtn.style.border = "none";
   delbtn.style.textDecoration = "none";
-  delbtn.style.backgroundColor = "whitesmoke";
+  delbtn.style.backgroundColor = "#FBFBFB";
   delbtn.classList.add("delete");
   let checkbox = document.createElement("input");
-  checkbox.setAttribute("type","checkbox");
+  checkbox.setAttribute("type", "checkbox");
   ul.appendChild(item);
   item.appendChild(delbtn);
   item.appendChild(checkbox);
-  checkbox.addEventListener("change",function(){
-    if(this.checked){
+  checkbox.addEventListener("change", function () {
+    if (this.checked) {
       this.parentElement.style.color = "green";
       item.style.textDecoration = "line-through";
     }
-    else{
+    else {
       this.parentElement.style.color = "red";
       item.style.textDecoration = "none";
     }
@@ -34,13 +37,13 @@ btn.addEventListener("click", function() {
   inp.value = "";
 });
 
-ul.addEventListener("click", function(event){
+ul.addEventListener("click", function (event) {
   console.dir(event.target.nodeName);
-  if(event.target.nodeName == "BUTTON"){
+  if (event.target.nodeName == "BUTTON") {
     let ListItem = event.target.parentElement;
     ListItem.remove();
   }
-  if(event.target.nodeName == "I"){
+  if (event.target.nodeName == "I") {
     let ListItem = event.target.parentElement.parentElement;
     ListItem.remove();
   }
